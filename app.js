c3e7a6b9116e1d2512c31ef2a4e22461fc8e1adf
silvana02 <silvanalima.89@hotmail.com>
2022-09-03 23:04:26 -0300

    agrego  encriptador en js

--- app.js
+++ app.js
@@ -1,21 +1,63 @@
-var a = "ai";
-var e = "enter";
-var i = "imes";
-var o = "ober";
-var u = "ufat";
-var input = document.querySelector("input");
-input.focus();
+var a = "a";
+var e = "e";
+var i = "i";
+var o = "o";
+var u = "u";
 
-function verificar() {
-  if (input.value == a) {
-    alert("Usted acertó");
-  } else {
-    alert("Usted erró");
+var textarea = document.querySelector('textarea')
+textarea.focus();
+var desencriptar = document.getElementById('txt-desencriptado')
+
+function encriptar(){
+
+var acc = ""
+
+ for(var j=0; j<textarea.value.length; j++){
+     
+  var letra = textarea.value[j]
+  
+    if (letra == a)  {
+    var letra = "ai"
+  } 
+  else if(letra== e){
+  var letra = "enter"
+  }
+  else if(letra== i){
+    var letra = "imes"
+  }
+  else if(letra== o){
+      var letra = "ober"
+  }  
+
+  else if(letra== u){
+    var letra = "ufat"
+    }
+
+
+  else{
+    var letra = textarea.value[j]
+
   }
 
-  input.value = "";
-  input.focus();
+ acc = acc + `${letra}`
+}
+desencriptar.innerHTML = acc;
+textarea.value = "";
+textarea.focus();
 }
 
+
+// var oculto =document.getElementById("cont-ocultar")
+
+// function ocultar (){
+
+//   oculto.style.display = "none";
+
+// }
+
+
+
 var button = document.querySelector("button");
-button.onclick = verificar;
+button.onclick = encriptar; ocultar;
+
+
