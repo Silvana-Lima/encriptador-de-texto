cf6625fa85d61b136df142995d12544f12f04fb3
silvana02 <silvanalima.89@hotmail.com>
2022-09-06 11:35:21 -0300

    agrego funciones: ocultar, desencriptar y copiar, modifico responsive

--- app.js
+++ app.js
@@ -1,63 +1,73 @@
-var a = "a";
-var e = "e";
-var i = "i";
-var o = "o";
-var u = "u";
 
-var textarea = document.querySelector('textarea')
+//////******* FUNCION OCULTAR MUÑECO ******//////
+
+function ocultar() {
+  document.getElementById("cont-ocultar").style.display = "none";
+  document.getElementById("cont-output").style.display = "block";
+}
+
+
+//////******* FUNCION ENCRIPTAR ******//////
+
+var textarea = document.querySelector("textarea");
 textarea.focus();
-var desencriptar = document.getElementById('txt-desencriptado')
+var mensaje = document.getElementById("txt-desencriptado");
 
-function encriptar(){
+function encriptar() {
+  var acc = "";
 
-var acc = ""
+  for (var j = 0; j < textarea.value.length; j++) {
+    var letra = textarea.value[j];
 
- for(var j=0; j<textarea.value.length; j++){
-     
-  var letra = textarea.value[j]
-  
-    if (letra == a)  {
-    var letra = "ai"
-  } 
-  else if(letra== e){
-  var letra = "enter"
-  }
-  else if(letra== i){
-    var letra = "imes"
-  }
-  else if(letra== o){
-      var letra = "ober"
-  }  
-
-  else if(letra== u){
-    var letra = "ufat"
+    if (letra == "a") {
+      var letra = "ai";
+    } else if (letra == "e") {
+      var letra = "enter";
+    } else if (letra == "i") {
+      var letra = "imes";
+    } else if (letra == "o") {
+      var letra = "ober";
+    } else if (letra == "u") {
+      var letra = "ufat";
+    } else {
+      var letra = textarea.value[j];
     }
 
-
-  else{
-    var letra = textarea.value[j]
-
+    acc = acc + `${letra}`;
   }
-
- acc = acc + `${letra}`
-}
-desencriptar.innerHTML = acc;
-textarea.value = "";
-textarea.focus();
+  mensaje.innerHTML = acc;
+  textarea.value = "";
+  textarea.focus();
 }
 
+//////******* FUNCION DESENCRIPTAR ******//////
 
-// var oculto =document.getElementById("cont-ocultar")
+function desencriptarBtn() {
+  const TextoAdesencriptar = desencriptarTxt(textarea.value);
+  mensaje.value = TextoAdesencriptar;
+  textarea.value = "";
+}
 
-// function ocultar (){
+function desencriptarTxt(stringDesencriptar) {
+  var reemplazar = [["a", "ai"],["e", "enter"],["i", "imes"],["o", "ober"],["u", "ufat"]];
 
-//   oculto.style.display = "none";
+  for (var z = 0; z < reemplazar.length; z++) {
+    if (stringDesencriptar.includes(reemplazar[z][1])) {
+      stringDesencriptar = stringDesencriptar.replaceAll(reemplazar[z][1],reemplazar[z][0]);
+    }
+  }
+  return stringDesencriptar;
+}
 
-// }
+////----****>>>>BOTON COPIAR<<<<****----////
 
+function copy() {
+  var copyText = document.querySelector("#txt-desencriptado");
+  copyText.select();
+  document.execCommand("copy");
+}
 
+document.querySelector("#copy").addEventListener("click", copy);
 
-var button = document.querySelector("button");
-button.onclick = encriptar; ocultar;
 
 
